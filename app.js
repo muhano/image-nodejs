78182f50aa4f049b9cca79d0703222d526f6d98b
heri setyawan <heri.nugroho@hanabank.co.id>
2023-12-14 08:26:20 +0700

    add log when server being requested

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('This a simple Nodejs image')
-})
+app.get("/", (req, res) => {
+  console.log("This app has been hit at " + new Date());
+  res.send("This a simple Nodejs image");
+});
 
 app.listen(port, () => {
-  console.log(`Nodejss app listening on port ${port}`)
-})
+  console.log(`NodeJS app listening on port ${port}`);
+});
